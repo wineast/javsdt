12743a6b135509b8dfb1195d3a1754a77ccd1d33
陈烨桦 <wxchenyehua@gf.com.cn>
2024-06-19 10:23:50 +0800

    下载图片时增加header信息，readme增加打包命令

--- javsdt/Functions/Requests/Download.py
+++ javsdt/Functions/Requests/Download.py
@@ -6,15 +6,23 @@ from PIL import Image
 # 下载图片，无返回
 # 参数：图片地址，存放路径，代理
 def download_pic(url, path, proxy):
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Mobile Safari/537.36',
+        'referer': url,
+    }
+
     for retry in range(5):
         try:
             if proxy:
-                r = requests.get(url, proxies=proxy, stream=True, timeout=(6, 10))
+                r = requests.get(url, proxies=proxy, stream=True, timeout=(6, 10), headers=headers)
                 with open(path, 'wb') as pic:
                     for chunk in r:
                         pic.write(chunk)
             else:
-                r = requests.get(url, stream=True, timeout=(6, 10))
+                r = requests.get(url, stream=True, timeout=(6, 10), headers=headers)
                 with open(path, 'wb') as pic:
                     for chunk in r:
                         pic.write(chunk)
